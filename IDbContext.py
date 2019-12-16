ef64c7bd28423ae9ca9b595036d8385126d7f771
vladimirpetukhov <vlado_web@abv.bg>
2019-12-16 19:38:30 +0200

    FIX: update bug

--- IDbContext.py
+++ IDbContext.py
@@ -9,26 +9,31 @@ class IDbContext:
             "CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, [name] text, customer text, retailer text, price text)")
         self.conn.commit()
 
-    
     def insert(self, part, customer, retailer, price):
         self.cur.execute("INSERT INTO parts VALUES (NULL, ?, ?, ?, ?)",
                          (part, customer, retailer, price))
         self.conn.commit()
-
 
     def fetch(self):
         self.cur.execute("SELECT * FROM parts")
         rows = self.cur.fetchall()
         return rows
 
-
     def remove(self, id):
         self.cur.execute("DELETE FROM parts WHERE id=?", (id,))
         self.conn.commit()
 
-    
-    def get_part(self,id):
+    def get_part(self, id):
         self.cur.execute("SELECT * FROM parts WHERE id=?", (id,))
-        
-        part=self.cur.fetchone()
+
+        part = self.cur.fetchone()
         return part
+        
+
+    def update(self, id, part, customer, retailer, price):
+        self.cur.execute("UPDATE parts SET [name] = ?, customer = ?, retailer = ?, price = ? WHERE id = ?",
+                         (part, customer, retailer, price, id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
