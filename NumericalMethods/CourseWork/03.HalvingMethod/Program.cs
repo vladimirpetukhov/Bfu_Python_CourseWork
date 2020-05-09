0c5afc6c68cc1bb1e5e1ef8e5b5bb9c3dbd5584c
vladimirpetukhov <vlado_web@abv.bg>
2020-05-09 14:24:18 +0300

    add halvingmethod

---
+++ NumericalMethods/CourseWork/03.HalvingMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.HalvingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
